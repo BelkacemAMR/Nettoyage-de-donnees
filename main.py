a26eda800ca9608b24292032132dbbf555b52857
Belkacem <abel-and-co@hotmail.com>
2023-06-29 21:40:41 +0200

    Version Finale

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import numpy as np
 # Chargement des données à partir d'un fichier CSV
 data = pd.read_csv('donnees_brutes.csv')
 
+
 # Suppression des valeurs manquantes
 data = data.dropna()
 
+
 # Élimination des doublons
 data = data.drop_duplicates()
+
 
 # Détection des valeurs aberrantes (ici, considérons les valeurs aberrantes comme étant les points de données situés à plus de 3 écarts-types de la moyenne)
 mean = np.mean(data['valeur'])
@@ -16,8 +19,10 @@ std = np.std(data['valeur'])
 threshold = 3 * std
 data = data[np.abs(data['valeur'] - mean) < threshold]
 
+
 # Normalisation des données (par exemple, mise à l'échelle des valeurs entre 0 et 1)
 data['valeur_normalisee'] = (data['valeur'] - data['valeur'].min()) / (data['valeur'].max() - data['valeur'].min())
+
 
 # Affichage des données nettoyées
 print(data)
